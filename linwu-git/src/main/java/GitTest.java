288ce8279ef8e8fb22457afd761204b5005cf35d
zhanglei <330118005@qq.com>
2022-07-11 23:36:03 +0800

    add

--- linwu-git/src/main/java/GitTest.java
+++ linwu-git/src/main/java/GitTest.java
@@ -1,6 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git test");
-        System.out.println("需要提交的东西");
+//        System.out.println("git test");
+//        System.out.println("需要提交的东西");
+        //这里搁置的东西
+
+        LocalDateTime parse = LocalDateTime.parse("20121123.1111", DateTimeFormatter.ofPattern("yyyyMMdd.HHmm"));
+        System.out.println(parse.toLocalDate().toString());
+        System.out.println(parse.toLocalTime().toString());
+
     }
 }
--- linwu-jvm/src/main/java/Test.java
+++ linwu-jvm/src/main/java/Test.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Test {
-    public static void main(String[] args) {
-        final String s = new Object().toString();
-        System.out.println(s);
+    public static void main(String[] args) throws Exception {
+        Path root = new File("/Users/zhanglei/soft/nacos").toPath().normalize();
+
+        Path usedProperties = Paths.get(root.resolve("conf/application.properties").toString());
+        Path copyProperties = Paths.get(root.resolve("conf/application.properties.copy").toString());
+
+        Files.delete(usedProperties);
+        Files.copy(copyProperties, usedProperties);
+
+        List<String> lines = Files.readAllLines(usedProperties);
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+
+
+        }
     }
 }
